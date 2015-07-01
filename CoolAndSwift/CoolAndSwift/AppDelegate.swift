642c3d95fc07d67350f43df59bd14de6344f6df8
Jonathan Lehr <jonathan@aboutobjects.com>
2015-07-01 17:47:37 -0400

    Stage 1: Create window and subviews

---
+++ CoolAndSwift/CoolAndSwift/AppDelegate.swift
@@ -0,0 +1,35 @@
+// Copyright (C) 2014 About Objects, Inc. All Rights Reserved.
+// See LICENSE.txt for this example's licensing information.
+//
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate
+{
+    var window: UIWindow?
+
+    func application(application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
+    {
+        var screenRect = UIScreen.mainScreen().bounds
+        window = UIWindow(frame: screenRect)
+        window?.backgroundColor = UIColor.lightGrayColor()
+        window?.makeKeyAndVisible()
+        
+        let backgroundView = UIView(frame: screenRect)
+        window?.addSubview(backgroundView)
+        
+        let view1 = UIView(frame: CGRect(x: 20, y: 40, width: 80, height: 30))
+        let view2 = UIView(frame: CGRect(x: 60, y: 100, width: 80, height: 30))
+        
+        backgroundView.addSubview(view1)
+        backgroundView.addSubview(view2)
+
+        backgroundView.backgroundColor = UIColor.brownColor()
+        view1.backgroundColor = UIColor.purpleColor()
+        view2.backgroundColor = UIColor.orangeColor()
+        
+        return true
+    }
+}
+
